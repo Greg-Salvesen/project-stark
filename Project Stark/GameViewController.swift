874d0996502b81109642a0917eba7778ab742e06
shadowarcher <salvesen@nationalgalactic.com>
2015-12-14 19:39:54 -0500

    Set up the GameScene

--- Project Stark/GameViewController.swift
+++ Project Stark/GameViewController.swift
@@ -10,10 +10,22 @@ import UIKit
 import SpriteKit
 
 class GameViewController: UIViewController {
+    
+    var scene: GameScene!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+     
+        // Configure the view
+        let skView = view as! SKView
+        skView.multipleTouchEnabled = true
         
+        // Create and configure the scene
+        scene = GameScene(size: skView.bounds.size)
+        scene.scaleMode = .AspectFill
+        
+        // Present the scene
+        skView.presentScene(scene)
     }
 
     override func shouldAutorotate() -> Bool {
